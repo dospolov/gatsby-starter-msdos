ad4c719dc348d00fad01d2ff4537b61ba7493489
Marat Dospolov <marat.dospolov@gmail.com>
2020-12-03 04:41:30 +0200

    ncu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-    defaultLineHeights: true,
-    standardFontWeights: true
-  },
   purge: ['./src/**/*.js', './src/**/*.jsx'],
   theme: {
     extend: {
