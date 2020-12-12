dd7488a15f11e741c47781c7beb7f849adf2bd12
Marat Dospolov <marat.dospolov@gmail.com>
2020-12-12 13:22:50 +0200

    cv fields

--- src/partials/CVMaker.js
+++ src/partials/CVMaker.js
@@ -7,42 +7,113 @@ import { navigate } from 'gatsby'
 
 function CVMaker() {
   const [state, setState] = useState({
-    email: ''
+    name: '',
+    position: '',
+    city: '',
+    email: '',
+    github: ''
   })
-  const createPage = () => {
-    console.log(state)
 
-    navigate('/generated-cv', {
-      state,
-      replace: true
-    })
-  }
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
         <div className="relative pt-10 pb-10">
           <div className="max-w-3xl mx-auto pb-12 md:pb-16">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Email
-              </label>
-              <div className="mt-1">
-                <input
-                  type="text"
-                  name="email"
-                  id="email"
-                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                  placeholder="you@example.com"
-                  value={state.email}
-                  onChange={({ target: { value } }) =>
-                    setState({ ...state, email: value })
-                  }
-                />
+              <input
+                type="text"
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md text-gray-900 mb-3"
+                placeholder="name"
+                value={state.name}
+                onChange={({ target: { value } }) => setState({ ...state, name: value })}
+              />
+              <input
+                type="text"
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md text-gray-900 mb-3"
+                placeholder="position"
+                value={state.position}
+                onChange={({ target: { value } }) =>
+                  setState({ ...state, position: value })
+                }
+              />
+              <input
+                type="text"
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md text-gray-900 mb-3"
+                placeholder="city"
+                value={state.city}
+                onChange={({ target: { value } }) => setState({ ...state, city: value })}
+              />
+              <input
+                type="text"
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md text-gray-900 mb-3"
+                placeholder="email"
+                value={state.email}
+                onChange={({ target: { value } }) => setState({ ...state, email: value })}
+              />
+              <input
+                type="text"
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md text-gray-900 mb-3"
+                placeholder="github"
+                value={state.github}
+                onChange={({ target: { value } }) =>
+                  setState({ ...state, github: value })
+                }
+              />
+
+              <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                <label
+                  htmlFor="cover_photo"
+                  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                >
+                  Cover photo
+                </label>
+                <div className="mt-2 sm:mt-0 sm:col-span-2">
+                  <div className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                    <div className="space-y-1 text-center">
+                      <svg
+                        className="mx-auto h-12 w-12 text-gray-400"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 48 48"
+                        aria-hidden="true"
+                      >
+                        <path
+                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                      <div className="flex text-sm text-gray-600">
+                        <label
+                          htmlFor="file-upload"
+                          className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                        >
+                          <span>Upload a file</span>
+                          <input
+                            id="file-upload"
+                            name="file-upload"
+                            type="file"
+                            className="sr-only"
+                          />
+                        </label>
+                        <p className="pl-1">or drag and drop</p>
+                      </div>
+                      <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+                    </div>
+                  </div>
+                </div>
               </div>
+
               <button
                 type="button"
                 className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                onClick={createPage}
+                onClick={() =>
+                  navigate('/generated-cv', {
+                    state,
+                    replace: true
+                  })
+                }
               >
                 Button text
               </button>
--- src/partials/GeneratedCV.js
+++ src/partials/GeneratedCV.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-function GeneratedCV({ state: { email } }) {
+function GeneratedCV({ state: { name, position, city, email, github } }) {
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
         <div className="relative pt-10 pb-10">
           <div className="max-w-3xl mx-auto pb-12 md:pb-16">{email}</div>
+          <div className="max-w-3xl mx-auto pb-12 md:pb-16">{city}</div>
         </div>
       </div>
     </section>
