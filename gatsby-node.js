0f7374d37338e3ffd9b9519d72daa60e8dfa65b2
Marat Dospolov <marat.dospolov@gmail.com>
2021-03-15 23:00:20 +0200

    ncu

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,14 @@
 exports.createPages = require('./gatsby/create-pages')
 // exports.onCreateNode = require('./gatsby/on-create-node')
 
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
-  if (stage.startsWith('develop')) {
-    actions.setWebpackConfig({
-      resolve: {
-        alias: {
-          'react-dom': '@hot-loader/react-dom'
-        }
-      }
-    })
-  }
-}
+// exports.onCreateWebpackConfig = ({ stage, actions }) => {
+//   if (stage.startsWith('develop')) {
+//     actions.setWebpackConfig({
+//       resolve: {
+//         alias: {
+//           'react-dom': '@hot-loader/react-dom'
+//         }
+//       }
+//     })
+//   }
+// }
